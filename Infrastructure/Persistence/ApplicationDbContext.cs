a6a0aa5da750dd3e2cea3b04f727f1a5bd7fedd0
Xhuliano <xhulianokoci@gmail.com>
2024-12-23 12:44:02 -0600

    Add project files.

---
+++ Infrastructure/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Persistence;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    //public DbSet<Test> YourEntities { get; set; }
+}
