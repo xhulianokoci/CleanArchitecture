deace1034b633d5262e7dc2460ddc046177f1277
Xhuliano <xhulianokoci@gmail.com>
2024-12-23 18:35:54 -0600

    added DB Context Factory

---
+++ Infrastructure/Persistence/ApplicationDbContextFactory.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace Infrastructure.Persistence;
+
+public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+{
+    public ApplicationDbContext CreateDbContext(string[] args)
+    {
+        // Build configuration
+        var configuration = new ConfigurationBuilder()
+                .SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), "../CleanArchitecture")) // Adjust path
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
+
+        // Create DbContext options
+        var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+        optionsBuilder.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+
+        return new ApplicationDbContext(optionsBuilder.Options);
+    }
+}
